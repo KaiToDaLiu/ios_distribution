a2206dd876584c00596b126df81a37f69b7081c6
KaiToDaLiu <weizhen@kaito.ai>
2024-10-14 10:27:29 +0800

    add

--- project/build/build/main.swift
+++ project/build/build/main.swift
@@ -23,48 +23,17 @@ Input Numbers:
 """
         print(hintStr, terminator: "")
         
+        let info = Info()
+        info.parse()
+        
         // Capture input from the user
         if let input = readLine() {
             if input.lowercased() == "1" {
                 shouldExit = false
-                let build = Build()
-                if var date = build.getBuildDate() {
-                    date = date.trimmingCharacters(in: .whitespacesAndNewlines)
-                    let gitPath = "https://KaiToDaLiu.github.io/ios_distribution/"
-                    print("=============================================")
-                    print(date) // 2024_09_26_14_56_09
-                    print("https://KaiToDaLiu.github.io/ios_distribution/build/" + date + "/KaiToApp.ipa")
-                    print("https://KaiToDaLiu.github.io/ios_distribution/57.png")
-                    print("https://KaiToDaLiu.github.io/ios_distribution/512.png")
-                    print("\n")
-                    print("=============================================")
-                }
+                info.logArchiveInformation()
             } else if input.lowercased() == "2" {
-                let currentPath = FileManager.default.currentDirectoryPath
-                print("Current path: \(currentPath)")
-                return
-                
                 shouldExit = false
-                let build = Build()
-                if var date = build.getBuildDate() {
-                    date = date.trimmingCharacters(in: .whitespacesAndNewlines)
-                    let manifestName = date + "/" + "manifest.plist"
-                    let manifestFullPath = "itms-services:///?action=download-manifest&url=https://KaiToDaLiu.github.io/ios_distribution/build/" + manifestName
-                    print("=============================================")
-                    print(manifestFullPath)
-                    // Change here!!!
-                    let destPath = "/Users/daliu_kt/Desktop/job/GitHub/ios_distribution/build/" + date + "/qrcode.jpg"
-                    QRCode().saveQRImage(from: manifestFullPath, path: destPath)
-                    print("insert div:")
-                    let div = """
-<div class="vertical">
-                <img src="build/\(date)/qrcode.jpg" alt="scan it with iphone camera">
-                \(Build.splitDate(date))
-            </div>
-"""
-                    print(div)
-                    print("=============================================")
-                }
+                info.logHTML()
             } else if input.lowercased() == "q" {
                 shouldExit = true
                 print("Bye")
