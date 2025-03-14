a6455a279f33615575b3825f6fe3f43a51e3bac4
KaiToDaLiu <weizhen@kaito.ai>
2025-03-14 15:51:02 +0800

    add

--- build/upload.sh
+++ build/upload.sh
@@ -44,23 +44,23 @@ upload_gitpage() {
 	echo "manifest full path: ${MANIFEST_FULL_PATH}"
 	echo "qr code path: ${MAC_TIME_DIR}/qrcode.jpg"
 
-	# echo "----------------------------------"
-	# echo "4. 提交到 GitHub"
-	# echo "----------------------------------"
+	echo "----------------------------------"
+	echo "👉 5. 提交到 GitHub Pages"
+	echo "----------------------------------"
 	cd ${ios_distribution}
-    echo "提交代码 ${ios_distribution}"
+    echo "提交代码 ${ios_distribution} --> ${GIT_PAGE_HOME}"
 	pwd
 	git add *
 	git commit -m "add new build ${CURRENT_TIME}"
 	git push
 
 	echo "----------------------------------"
-	echo "👉 5. Set build description"
+	echo "👉 6. Set build description"
 	echo "----------------------------------"
 	cd ${project_dir}
 	pwd
 	# echo DESC_INFO:${BASE_URL}/qrcode.png,${BASE_URL}/app-debug.apk
-	echo "DESC_INFO:${MAC_TIME_DIR}/qrcode.jpg,${MANIFEST_FULL_PATH}"
+	# echo "DESC_INFO:${MAC_TIME_DIR}/qrcode.jpg,${MANIFEST_FULL_PATH}"
 #    cp ${MAC_TIME_DIR}/qrcode.jpg ./qrcode.jpg # 否则 img src 不显示
 #    echo "DESC_INFO:./qrcode.jpg,${MANIFEST_FULL_PATH}"
 #    # echo "DESC_INFO:${project_dir}/qrcode.jpg,${MANIFEST_FULL_PATH}"
