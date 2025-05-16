06ce073c8c3f417a4002722bbeeebd589192a75c
KaiToDaLiu <weizhen@kaito.ai>
2025-05-16 16:29:25 +0800

    add

--- project/build/build/Info.swift
+++ project/build/build/Info.swift
@@ -18,7 +18,7 @@ class Info {
     let gitPageHome = "https://KaiToDaLiu.github.io/ios_distribution/"
     
     /// 2024_09_26_14_56_09
-    private var ipaDirName = "2025_05_16_15_17_09" // HERE!!
+    private var ipaDirName = "2025_05_16_16_26_09" // HERE!!
     /// 2024-09-26 14:56:09
     private var formatDate = ""
     /// Debug or Release
